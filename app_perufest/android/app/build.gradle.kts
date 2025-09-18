89daaefe8d55c73430a03dc6a84b9cfecb328b2a
Andree Flores Melendez <af2017057494@virtual.upt.pe>
2025-09-18 11:30:22 -0500

    Sprint 1 - Recuperar Contraseña

--- app_perufest/android/app/build.gradle.kts
+++ app_perufest/android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "com.example.app_perufest"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23  // Actualizado para Firebase Auth
+        minSdk = flutter.minSdkVersion  // Actualizado para Firebase Auth
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
